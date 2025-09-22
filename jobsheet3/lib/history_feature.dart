06405a6e6ff6b9086f11ec7b66eeae6409b2a796
tasyasss <tasyasakina0703@gmail.com>
2025-09-23 00:11:09 +0700

    branch history-feature

---
+++ jobsheet3/lib/history_feature.dart
@@ -0,0 +1,6 @@
+void tampilkanRiwayat(List<Map<String, dynamic>> riwayat) {
+  for (var data in riwayat) {
+    print("Tinggi: ${data['tinggi']} cm, Berat: ${data['berat']} kg, "
+        "BMI: ${data['bmi'].toStringAsFixed(2)}, Kategori: ${data['kategori']}");
+  }
+}
