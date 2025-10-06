7facd864b54dc233cce360604f1e448805a04a39
tasyasss <tasyasakina0703@gmail.com>
2025-10-06 21:17:19 +0700

    add method column _buildButtonColumn

--- FlutterGitbook/6_Layout_dan_Navigasi/layout_flutter/lib/main.dart
+++ FlutterGitbook/6_Layout_dan_Navigasi/layout_flutter/lib/main.dart
@@ -61,4 +61,26 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
+
+  // Method pembantu untuk membuat kolom tombol
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
 }
