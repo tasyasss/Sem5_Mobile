bec4ab2d5f12983d48bab91028387530aa38730d
tasyasss <tasyasakina0703@gmail.com>
2025-10-13 23:30:04 +0700

    add widget di main.dart

--- FlutterGitbook/7_Manajemen_Plugin/flutter_plugin_pubdev/lib/main.dart
+++ FlutterGitbook/7_Manajemen_Plugin/flutter_plugin_pubdev/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'red_text_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -86,25 +87,22 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+            Container(
+              color: Colors.yellowAccent,
+              width: 50,
+              child: const RedTextWidget(
+                text: 'You have pushed the button this many times:',
+              ),
+            ),
+            Container(
+              color: Colors.greenAccent,
+              width: 100,
+              child: const Text('You have pushed the button this many times:'),
+            ),
+            // Tambahkan kode ini agar angka counter muncul
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
