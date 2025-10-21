3d78d76822c73b32df0043c01797a77e0ed5d8cc
tasyasss <tasyasakina0703@gmail.com>
2025-10-21 16:44:48 +0700

    UTS: Selesai Soal 3 - Implementasi Flutter TTS

--- FlutterGitbook/7_Aplikasi_OCR/ocr_sederhana/lib/screens/result_screen.dart
+++ FlutterGitbook/7_Aplikasi_OCR/ocr_sederhana/lib/screens/result_screen.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
 import 'home_screen.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 
-class ResultScreen extends StatelessWidget {
+class ResultScreen extends StatefulWidget {
   final String ocrText;
 
-  const ResultScreen({Key? key, required this.ocrText}) : super(key: key);
+  const ResultScreen({super.key, required this.ocrText});
+
+  @override
+  State<ResultScreen> createState() => _ResultScreenState();
+}
+
+class _ResultScreenState extends State<ResultScreen> {
+  late FlutterTts _flutterTts;
+
+  @override
+  void initState() {
+    super.initState();
+    _flutterTts = FlutterTts();
+    _flutterTts.setLanguage("id-ID");
+  }
+
+  @override
+  void dispose() {
+    _flutterTts.stop();
+    super.dispose();
+  }
+
+  Future<void> _speak() async {
+    if (widget.ocrText.isEmpty) return;
+    await _flutterTts.stop();
+    await _flutterTts.speak(widget.ocrText);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,20 +41,36 @@ class ResultScreen extends StatelessWidget {
         padding: const EdgeInsets.all(16.0),
         child: SingleChildScrollView(
           child: SelectableText(
-            ocrText.isEmpty ? 'Tidak ada teks ditemukan.' : ocrText,
+            widget.ocrText.isEmpty
+                ? 'Tidak ada teks ditemukan.'
+                : widget.ocrText,
             style: const TextStyle(fontSize: 18),
           ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.home),
-        onPressed: () {
-          Navigator.pushAndRemoveUntil(
-            context,
-            MaterialPageRoute(builder: (context) => const HomeScreen()),
-            (route) => false,
-          );
-        },
+      floatingActionButton: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          FloatingActionButton(
+            heroTag: 'ttsSpeak',
+            child: const Icon(Icons.volume_up),
+            onPressed: _speak,
+            tooltip: 'Bacakan Teks',
+          ),
+          const SizedBox(height: 12),
+          FloatingActionButton(
+            heroTag: 'goHome',
+            child: const Icon(Icons.home),
+            onPressed: () {
+              Navigator.pushAndRemoveUntil(
+                context,
+                MaterialPageRoute(builder: (_) => const HomeScreen()),
+                (route) => false,
+              );
+            },
+            tooltip: 'Kembali ke Home',
+          ),
+        ],
       ),
     );
   }
